c10b6fa7bf542f6449dde143196dbf7aa456d3e5
苗海洋 <2605319346@qq.com>
2021-05-19 11:15:24 +0800

    update 四则算术PRO_结对编程/Ari_main.java. 创建可视化界面

--- 四则算术PRO_结对编程/Ari_main.java
+++ 四则算术PRO_结对编程/Ari_main.java
@@ -3,11 +3,8 @@ package arithmetic;
 public class Ari_main {
 
 	public static void main(String[] args) {
-        
-		//创建可视化界面
-		Jfra jf = new Jfra();
-		
-		//测试dupCheck(string,string)即字符串查重功能)
+		Jfra jf = new Jfra();	
+		//测试dupCheck(string,string),即字符串查重功能)
 		/*DupCheck dw = new DupCheck();
 		String str1 = "1'1/1 + 2/1 + 3";
 		String str2 = "2/1 +  1'1/1 + 3 ";
